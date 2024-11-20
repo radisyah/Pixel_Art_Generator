0cfc151b4dfac9ac33b83d096ae1885bd680589e
radisyah <syahputradimas965@gmail.com>
2024-11-20 14:09:00 +0700

    first

---
+++ .history/index_20241120125322.js
@@ -0,0 +1,6 @@
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit-grid");
+let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
+let gridHeight = document.getElementById("height-range");
+let colorButton = document.getElementById("color-input");
---
+++ .history/index_20241120125623.js
@@ -0,0 +1,22 @@
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit-grid");
+let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
+let gridHeight = document.getElementById("height-range");
+let colorButton = document.getElementById("color-input");
+let eraseBtn = document.getElementById("erase-btn");
+let paintBtn = document.getElementById("paint-btn");
+let widthValue = document.getElementById("widht-value");
+let heightValue = document.getElementById("height-value");
+
+let events = {
+    mouse: {
+        down: "mousedown",
+        move: "mousemove",
+        up: "mouseup"
+    },
+    touch: {
+        down: "touchstart",
+        mobe: "touchmove",
+    }
+};
